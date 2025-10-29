36402a3118ea468d4ca279e59b08f559e6c42fbe
Gagego Maciel <gagego.maciel@gmail.com>
2025-10-29 08:59:37 -0300

    Refatorando codigo e aplicando clean code

--- src/components/ModalConfirmation/index.tsx
+++ src/components/ModalConfirmation/index.tsx
@@ -1,36 +1,49 @@
 import * as S from './styles'
+import Modal from 'react-native-modal'
 
 interface IModalConfirmationProps {
+  isVisible: boolean
   onPressCancel?: () => void
   onPressConfirm?: () => void
+  onBackdropPress?: () => void
 }
 
 export const ModalConfirmation: React.FC<IModalConfirmationProps> = ({
+  isVisible,
+  onBackdropPress,
   onPressCancel,
   onPressConfirm,
 }) => {
   return (
-    <S.Container>
-      <S.Header>
-        <S.Title>{'Deseja limpar tudo?'}</S.Title>
-      </S.Header>
+    <Modal
+      isVisible={isVisible}
+      onBackdropPress={onBackdropPress}
+      animationInTiming={500}
+      animationOutTiming={600}
+      useNativeDriver={true}
+    >
+      <S.Container>
+        <S.Header>
+          <S.Title>{'Deseja limpar tudo?'}</S.Title>
+        </S.Header>
 
-      <S.DescriptionWrapper>
-        <S.DescriptionText>
-          {
-            'Todos os itens da lista serão apagados. Você deseja realmente prosseguir com a remoção?'
-          }
-        </S.DescriptionText>
-      </S.DescriptionWrapper>
+        <S.DescriptionWrapper>
+          <S.DescriptionText>
+            {
+              'Todos os itens da lista serão apagados. Você deseja realmente prosseguir com a remoção?'
+            }
+          </S.DescriptionText>
+        </S.DescriptionWrapper>
 
-      <S.ButtonWrapper>
-        <S.Button type="CANCEL" onPress={onPressCancel}>
-          <S.ButtonText>{'Cancelar'}</S.ButtonText>
-        </S.Button>
-        <S.Button type="CONFIRM" onPress={onPressConfirm}>
-          <S.ButtonText>{'Continuar'}</S.ButtonText>
-        </S.Button>
-      </S.ButtonWrapper>
-    </S.Container>
+        <S.ButtonWrapper>
+          <S.Button type="CANCEL" onPress={onPressCancel}>
+            <S.ButtonText>{'Cancelar'}</S.ButtonText>
+          </S.Button>
+          <S.Button type="CONFIRM" onPress={onPressConfirm}>
+            <S.ButtonText>{'Continuar'}</S.ButtonText>
+          </S.Button>
+        </S.ButtonWrapper>
+      </S.Container>
+    </Modal>
   )
 }
--- src/components/Toast/styles.ts
+++ src/components/Toast/styles.ts
@@ -9,12 +9,12 @@ export const Container = styled.View`
 export const Content = styled.View`
   padding: 8px 16px;
   border-radius: 24px;
-  background: #27ae60;
+  background: ${({ theme }) => theme.colors.backgroundSuccess};
   margin-bottom: 16px;
 `
 
 export const Label = styled.Text`
   font-size: 12px;
   font-weight: 500;
-  color: #ffffff;
+  color: ${({ theme }) => theme.colors.textWhite};
 `
